dec3f00d20e87e9392ff04922a4729e3eea9883b
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-01-19 09:44:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your code here
+function change() {
+	let text=document.getElementById("text");
+	text.innerHTML("Accio ipsum dolor sit amet, join course, solve assignments, get placed above 5 lpa and live life king size.");
+}
